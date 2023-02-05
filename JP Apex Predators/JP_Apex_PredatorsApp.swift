3b92e8e75c20fc3f6ab01e23d63a248d8681799c
genabdulrehman <76816147+genabdulrehman@users.noreply.github.com>
2023-02-05 11:49:29 +0500

    Initial Commit

---
+++ JP Apex Predators/JP_Apex_PredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JP_Apex_PredatorsApp.swift
+//  JP Apex Predators
+//
+//  Created by Abdul Rehman on 05/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct JP_Apex_PredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
